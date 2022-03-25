6373883c1d50908ce22a3b3a50c44c2940d7ce9f
Anderson Souza <andersonszdc@gmail.com>
2022-03-25 15:13:13 -0300

    finished tab-navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,99 @@
-import React, { useEffect, useState } from "react";
+import React, {
+  createRef,
+  forwardRef,
+  Ref,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
+import { motion } from "framer-motion";
 import "./App.css";
 
+const items = [
+  { index: 1, text: "A" },
+  { index: 2, text: "B" },
+  { index: 3, text: "C" },
+];
+
 function App() {
-  const [activeTab, setActiveTab] = useState(1);
+  const [activeTab, setActiveTab] = useState(items[0].index);
+  const tabRefs = items.reduce((acc: any, item) => {
+    acc[item.index] = createRef();
+    return acc;
+  }, {});
+
+  console.log("render app");
+
   return (
     <div className="App">
-      <div className="wrapper">
-        <Tab
-          index={1}
-          text="A"
-          activeTab={activeTab}
-          setActiveTab={setActiveTab}
-        />
-        <Tab
-          index={2}
-          text="B"
-          activeTab={activeTab}
-          setActiveTab={setActiveTab}
-        />
-        <Tab
-          index={3}
-          text="C"
-          activeTab={activeTab}
-          setActiveTab={setActiveTab}
-        />
-        <span className="slider">
-          <span className="pointer" data-tab={activeTab} />
-        </span>
+      <div className="tabs">
+        {items.map((item, index) => (
+          <Tab
+            key={index}
+            ref={tabRefs[item.index]}
+            index={item.index}
+            text={item.text}
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+        ))}
       </div>
+      <Underline activeTab={activeTab} refs={tabRefs} />
     </div>
   );
 }
 
 export default App;
 
-const Tab = ({ text, index, activeTab, setActiveTab }: any) => {
-  const [classname, setClassname] = useState("");
-  useEffect(() => {
-    if (activeTab === index) {
-      setClassname("tab active");
-    } else {
-      setClassname("tab");
+const Tab = forwardRef(
+  ({ text, index, activeTab, setActiveTab }: any, ref: Ref<HTMLDivElement>) => {
+    const [classname, setClassname] = useState("tab");
+
+    useEffect(() => {
+      if (activeTab === index) {
+        setClassname("tab active");
+      } else {
+        setClassname("tab");
+      }
+    }, [activeTab, index]);
+
+    const handleClick = () => {
+      setActiveTab(index);
+    };
+
+    console.log("render tab");
+
+    return (
+      <div ref={ref} onClick={handleClick} className={classname}>
+        {text}
+      </div>
+    );
+  }
+);
+
+const Underline = ({ activeTab, refs }: any) => {
+  const [x, setAttributes] = useState(0);
+
+  const updateAttributes = useCallback(() => {
+    console.log(refs[activeTab]);
+    if (refs[activeTab]) {
+      setAttributes(refs[activeTab].current?.clientWidth);
     }
-  }, [activeTab, index]);
-  const handleClick = () => {
-    setActiveTab(index);
-  };
+  }, [activeTab, refs]);
+
+  useEffect(() => {
+    updateAttributes();
+  }, [refs, activeTab, updateAttributes]);
+
+  useEffect(() => {
+    console.log(refs);
+  }, [refs]);
+
+  console.log("render underline");
+
   return (
-    <div onClick={handleClick} className={classname}>
-      {text}
-    </div>
+    <span className="underline">
+      <motion.span animate={{ left: (activeTab - 1) * x }} className="pointer" />
+    </span>
   );
 };
